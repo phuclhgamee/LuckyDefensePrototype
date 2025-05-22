9131e83000af826bec7b92ea5f31bd98c03f4265
phuclhgamee <phuclh@gameestudio.com>
2025-05-22 16:55:45 +0700

    feat: restart button on click function

--- Assets/Scripts/GameOverPanel.cs
+++ Assets/Scripts/GameOverPanel.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace LuckyDenfensePrototype
 {
     public class GameOverPanel : MonoBehaviour
     {
+        [Header("UI")]
         [SerializeField] private TextMeshProUGUI text;
         [SerializeField] private Button restartButton;
+        
+        [Header("Reset components")]
+        [SerializeField] private IntegerVariable currentWaveVariable;
+        [SerializeField] private TileManager tileManager;
+        [SerializeField] private GuardianManager guardianManager;
+
         private void OnEnable()
         {
-            
+            restartButton.onClick.RemoveAllListeners();
+            restartButton.onClick.AddListener(()=>OnClickRestartButton());
         }
-        
+
+        private void OnClickRestartButton()
+        {
+            currentWaveVariable.Value = 0;
+            StopAllCoroutines();
+            Time.timeScale = 1;
+            SceneManager.LoadScene(0);
+        }
     }
 }
