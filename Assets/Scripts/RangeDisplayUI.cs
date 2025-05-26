9f926b97f644b8acf4f7b275d1ed0605ce7f4dc7
phuclhgamee <phuclh@gameestudio.com>
2025-05-26 14:45:39 +0700

    feat: sell guardian button

--- Assets/Scripts/RangeDisplayUI.cs
+++ Assets/Scripts/RangeDisplayUI.cs
@@ -18,24 +18,17 @@ namespace LuckyDenfensePrototype
                 float range = guardian.range;
                 
                 Camera cam = Camera.main;
-
-                // Lấy vị trí nhân vật (giữa vòng tròn)
+                
                 Vector3 worldPosCenter = tile.transform.position;
-
-                // Lấy điểm cách range đơn vị theo trục X (hoặc bất kỳ hướng nào)
+                
                 Vector3 worldPosEdge = worldPosCenter + Vector3.right * range;
-
-                // Chuyển 2 điểm sang màn hình (pixel)
+                
                 Vector3 screenCenter = cam.WorldToScreenPoint(worldPosCenter);
                 Vector3 screenEdge = cam.WorldToScreenPoint(worldPosEdge);
-
-                // Tính đường kính bằng pixel
+                
                 float diameterInPixels = Vector2.Distance(screenCenter, screenEdge) * 2f;
-
-                // Đặt kích thước vòng tròn UI
+                
                 rangeCircleImage.rectTransform.sizeDelta = new Vector2(diameterInPixels, diameterInPixels);
-
-                // (Tùy canvas loại gì mà cần đặt vị trí đúng nữa – ví dụ nếu canvas dạng World Space thì xử lý khác)
             }
         }
     }
---
+++ Assets/Scripts/SellButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace LuckyDenfensePrototype
+{
+    public class SellButton : MonoBehaviour
+    {
+        [SerializeField] private TileManager tileManager;
+        [SerializeField] private GuardianManager guardianManager;
+        [SerializeField] private Tile tile;
+        
+        [Header("Events")]
+        [SerializeField] private Event OnEnableMergeButtonEvent;
+        [SerializeField] private Event OnCloseTileUIEvent;
+        
+        public void OnClick()
+        {
+            Guardian g = tile.standingGuardians.Last();
+            tile.ClearGuardian(g);
+            guardianManager.summonedGuardians.Remove(g);
+            //tile.standingGuardians.Remove(g);
+            // for (int i = 0; i < tile.transform.childCount; i++)
+            // {
+            //     if (i == tile.transform.childCount - 1)
+            //     {
+            //         Destroy(tile.transform.GetChild(i).gameObject);
+            //     }
+            // }
+            OnCloseTileUIEvent.Raise();
+        }
+    }
+}
